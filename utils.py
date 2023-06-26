2ad67f36f0f89dc404c46e47612dbaa437758ed3
ghostjusr <97528884+ghostjusr@users.noreply.github.com>
2023-06-26 08:26:03 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,23 @@
+# utils.py
+# –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+# Word and TermColor utils
+# Use utils.TermColors["COLOR, REVERSE, NORMAL, BOLD, RESET, OR CLEARSCREEN GOES HERE"] 
+# as a variable with the print() command
+# TermColors["CLEARSCREEN"] completly clears the terminal
+# Set INTERNET_DOWNLOAD to True if you have internet
+# Set INTERNET_DOWNLOAD to False if you don't have internet
+# If INTERNET_DOWNLOAD is False, set filename to the location of a dictionary file (A file with words, 1 per line)
+# At the bottom it has the words = w.wordlist() function with the parameter for 'remove_proper_nouns'.
+# Change this to either True or False to remove proper nouns or not.
+#
+# FUNCTIONS
+# –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+# WordDict.wordlist(remove_proper_nouns: bool) - Returns a downloaded wordlist based on the INTERNET_DOWNLOAD variable and the first parameter
+
 import string
 import requests
 import random
+filename = ''
 
 TermColors = {
     "RED": "\033[1;31m",
@@ -17,7 +34,7 @@ TermColors = {
     "CLEARSCREEN": "\033[H\033[J",
 }
 
-INTERNET_DOWNLOAD = False
+INTERNET_DOWNLOAD = True
 
 
 class WordDict:
@@ -30,13 +47,12 @@ class WordDict:
         self.vowels = set("aeiou")
         self.consenents = self.alphabet.difference(self.vowels)
 
-    def download_wordlist(self, wordlen: int = 5, remove_proper_nouns: bool = True):
+    def wordlist(self, remove_proper_nouns: bool = True):
         if INTERNET_DOWNLOAD:
             url = "https://svnweb.freebsd.org/csrg/share/dict/words?revision=61569&view=co"
             r = requests.get(url, headers={'User-Agent': 'Custom'})
             wordlist = wordlist = r.content.decode('utf=8').split()
         else:
-            filename = '/Users/stauffer/dev/stauffer/StaufferPlayground/PythonPlay/games/words/dict-eng-61k.txt'
             with open(filename, 'r') as f:
                 wordlist = f.read().split()
         wordlist = [word for word in wordlist if len(word) == wordlen]
@@ -50,3 +66,6 @@ class WordDict:
 
     def __len__(self):
         return len(self.wordlist)
+
+w = WordDict()
+words = w.wordlist(remove_proper_nouns=True)
